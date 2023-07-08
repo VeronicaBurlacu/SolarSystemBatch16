fc0abfbe1c86145310106b00c83d8e419fbdf8b8
VeronicaBurlacu <veronica36890@gmail.com>
2023-07-08 13:09:54 -0500

    I have added sunt but some errors to be resolved

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
         Omnax.planets="2";
         Omnax.star="4";
 
+        FeatureSun AdamX=new FeatureSun();
+        AdamX.name="poya";
+        AdamX.temp=12344231 celcius;
+        AdamX.size="32942Km";
     }
 }
